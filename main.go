524f8f82fa04976b02607f58f4d3354b6cc0bed6
flycmd <pinkcmd@gmail.com>
2021-11-10 23:11:02 +0800

    ✨feat:重构中间件

--- main.go
+++ main.go
@@ -2,10 +2,10 @@ package main
 
 import (
 	"database/sql"
+	"goblog/app/http/middlewares"
 	"goblog/bootstrap"
 	"goblog/pkg/database"
 	"net/http"
-	"strings"
 
 	_ "github.com/go-sql-driver/mysql"
 	"github.com/gorilla/mux"
@@ -14,27 +14,6 @@ import (
 var router *mux.Router
 var db *sql.DB
 
-func forceHTMLMiddleware(next http.Handler) http.Handler {
-	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		// 设置标头
-		w.Header().Set("Content-Type", "text/html; charset=utf-8")
-		// 2. 继续处理请求
-		next.ServeHTTP(w, r)
-	})
-}
-
-func removeTrailingSlash(next http.Handler) http.Handler {
-	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		// 1. 除首页外，移除所有路径末尾的反斜杠
-		if r.URL.Path != "/" {
-			r.URL.Path = strings.TrimSuffix(r.URL.Path, "/")
-		}
-
-		// 2. 将请求传递下去
-		next.ServeHTTP(w, r)
-	})
-}
-
 func main() {
 	database.Initialize()
 	db = database.DB
@@ -42,8 +21,5 @@ func main() {
 	bootstrap.SetupDB()
 	router = bootstrap.SetupRoute()
 
-	// 中间件：强制内容类型为 HTML
-	router.Use(forceHTMLMiddleware)
-
-	http.ListenAndServe(":1001", removeTrailingSlash(router))
+	http.ListenAndServe(":1001", middlewares.RemoveTrailingSlash(router))
 }
