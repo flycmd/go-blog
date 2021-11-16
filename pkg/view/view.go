432b4c1cc0673e9ae13c949743239b390e91f7bd
flycmd <pinkcmd@gmail.com>
2021-11-16 22:05:54 +0800

    ✨feat:注册页面

--- pkg/view/view.go
+++ pkg/view/view.go
@@ -9,8 +9,21 @@ import (
 	"text/template"
 )
 
-// Render 渲染试图
+// D 是 map[string]interface{} 的简写
+type D map[string]interface{}
+
+// Render 渲染通用视图
 func Render(w io.Writer, data interface{}, tplFiles ...string) {
+	RenderTemplate(w, "app", data, tplFiles...)
+}
+
+// RenderSimple 渲染简单的视图
+func RenderSimple(w io.Writer, data interface{}, tplFiles ...string) {
+	RenderTemplate(w, "simple", data, tplFiles...)
+}
+
+// RenderTemplate 渲染试图
+func RenderTemplate(w io.Writer, name string, data interface{}, tplFiles ...string) {
 	// 1. 设置模板相对路径
 	viewDir := "resources/views/"
 
@@ -23,16 +36,16 @@ func Render(w io.Writer, data interface{}, tplFiles ...string) {
 	layoutFiles, err := filepath.Glob(viewDir + "layouts/*.gohtml")
 	logger.LogError(err)
 
-	// 4. 在 SLice 里新增我们的目标文件
+	// 4. 合并所有文件
 	allFiles := append(layoutFiles, tplFiles...)
 
-	// 5. 解析所有模板文件
+	// 5 解析所有模板文件
 	tmpl, err := template.New("").
 		Funcs(template.FuncMap{
 			"RouteName2URL": route.Name2URL,
 		}).ParseFiles(allFiles...)
 	logger.LogError(err)
 
-	// 6. 渲染模板
-	tmpl.ExecuteTemplate(w, "app", data)
+	// 6 渲染模板
+	tmpl.ExecuteTemplate(w, name, data)
 }
