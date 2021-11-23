cbf30c6a439913224de7e79058380f6bb8a6203a
flycmd <pinkcmd@gmail.com>
2021-11-23 23:04:00 +0800

    ✨feat:登录状态

--- routes/web.go
+++ routes/web.go
@@ -12,18 +12,15 @@ import (
 func RegisterWebRoutes(r *mux.Router) {
 	// 静态页面
 	pc := new(controllers.PagesController)
-
 	// 自定义 404 页面
 	r.NotFoundHandler = http.HandlerFunc(pc.NotFound)
-
-	r.HandleFunc("/", pc.Home).Methods("GET").Name("home")
 	r.HandleFunc("/about", pc.About).Methods("GET").Name("about")
 
 	// 文章相关页面
 	ac := new(controllers.ArticlesController)
 	r.HandleFunc("/articles/{id:[0-9]+}", ac.Show).Methods("GET").Name("articles.show")
 
-	r.HandleFunc("/articles", ac.Index).Methods("GET").Name("articles.index")
+	r.HandleFunc("/", ac.Index).Methods("GET").Name("articles.index")
 
 	r.HandleFunc("/articles", ac.Store).Methods("POST").Name("articles.store")
 	r.HandleFunc("/articles/create", ac.Create).Methods("GET").Name("articles.create")
